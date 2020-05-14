d7277d11ac243c9d8efb0bf4cb3976141b63775c
FlorSalcedoWD <florenciasalcedo13@gmail.com>
2020-05-14 13:37:06 -0300

    First commit

---
+++ src/views/Productos.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="productos">
+    <b-container class="mt-5">
+      <b-row>
+        <b-col>
+          <div
+            class="cards mt-4 mb-4"
+            v-for="instrumento in instrumentosData"
+            :key="instrumento.id"
+          >
+            <instrumento-item :instrumentoParam="instrumento"></instrumento-item>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import CardInstrumento from "@/components/CardInstrumento.vue";
+
+export default {
+  name: "Productos",
+  components: {
+    "instrumento-item": CardInstrumento
+  },
+  mounted() {
+    this.getInstrumentos();
+  },
+  data() {
+    return {
+      instrumentosData: []
+    };
+  },
+  methods: {
+    async getInstrumentos() {
+      const res = await fetch("/data/instrumentos.json");
+      const resJson = await res.json();
+      console.log(resJson);
+      this.instrumentosData = resJson.instrumentos;
+    }
+  }
+};
+</script>
